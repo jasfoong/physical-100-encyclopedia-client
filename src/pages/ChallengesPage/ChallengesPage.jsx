befa9f0e025f2cbe695d1bdce53d9974a5fe2d49
Jasmine Foong <jasfoong19@gmail.com>
2024-06-02 19:21:48 -0700

    display all contestants and challenges with basic mobile styling

--- src/pages/ChallengesPage/ChallengesPage.jsx
+++ src/pages/ChallengesPage/ChallengesPage.jsx
@@ -1,12 +1,59 @@
-import './ChallengesPage.scss'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { axiosInstance } from '../../utils/apiClient'
 import Header from '../../components/Header/Header'
-
+import './ChallengesPage.scss'
 
 const ChallengesPage = () => {
-    return (
+    const [challenges, setChallenges] = useState([]);
+    const [fetchError, setFetchError] = useState(false);
+
+    useEffect(() => {
+            const fetchChallenges = async () => {
+                try {
+                    const { data } = await axiosInstance.get("/challenges");
+                    setChallenges(data);
+                } catch (error) {
+                    console.log(`Error retrieving challenges`, error);
+                    setFetchError(true);
+                }
+            };
+            fetchChallenges();
+        }, []);
+        
+        if (challenges.length === 0) {
+            return <h3>Loading...</h3>
+        }
+
+        if (fetchError === true) {
+            return <p>Sorry, our servers are having a hard time retrieving the challenges. Please come back later.</p>
+        }
+
+        return (
         <>
         <Header />
-        <h1>Challenges Page</h1>
+        <section className="challenges">
+        <h1 className="challenges__heading">Challenges</h1>
+        <div className="challenges__column-headings-wrapper-lg">
+            <h2 className="challenges__column-heading-lg challenges__column-heading-lg--title">Title</h2>
+            <h2 className="challenges__column-heading-lg">Season</h2>
+            <h2 className="challenges__column-heading-lg">Team/Solo</h2>
+        </div>
+        {
+            challenges.map((challenge) => {
+                return (
+                <Link to={`/challenges/${challenge.id}`} key={challenge.id} className="challenges__instance-link">
+                    <div className="challenges__instance">
+                    <img className="challenges__img" src={`${process.env.REACT_APP_SERVER_URL}/${challenge.photo}`} alt="challenge context"/>
+                    <h3 className="challenges__text challenges__name">{challenge.name}</h3>
+                    <h4 className="challenges__text">Season {challenge.season}</h4>
+                    <h4 className="challenges__text">{challenge.team ? "Team" : "Solo"}</h4>
+                </div>
+                </Link>
+                )
+            })
+        }
+        </section>
         </>
     )
 }
--- src/pages/ContestantsPage/ContestantsPage.jsx
+++ src/pages/ContestantsPage/ContestantsPage.jsx
@@ -1,11 +1,59 @@
-import './ContestantsPage.scss'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { axiosInstance } from '../../utils/apiClient'
 import Header from '../../components/Header/Header'
+import './ContestantsPage.scss'
 
 const ContestantsPage = () => {
+    const [contestants, setContestants] = useState([]);
+    const [fetchError, setFetchError] = useState(false);
+
+    useEffect(() => {
+            const fetchContestants = async () => {
+                try {
+                    const { data } = await axiosInstance.get("/contestants");
+                    setContestants(data);
+                } catch (error) {
+                    console.log(`Error retrieving contestants`, error);
+                    setFetchError(true);
+                }
+            };
+            fetchContestants();
+        }, []);
+        
+        if (contestants.length === 0) {
+            return <h3>Loading...</h3>
+        }
+
+        if (fetchError === true) {
+            return <p>Sorry, our servers are having a hard time retrieving the challenges. Please come back later.</p>
+        }
+
     return (
         <>
         <Header />
-        <h1>Contestants Page</h1>
+        <section className="contestants">
+        <h1 className="contestants__heading">Contestants</h1>
+        <div className="contestants__column-headings-wrapper-lg">
+            <h2 className="contestants__column-heading-lg contestants__column-heading-lg--name">Name</h2>
+            <h2 className="contestants__column-heading-lg">Season</h2>
+            <h2 className="contestants__column-heading-lg">Career</h2>
+        </div>
+        {
+            contestants.map((contestant) => {
+                return (
+                <Link to={`/contestants/${contestant.id}`} key={contestant.id} className="contestants__instance-link">
+                    <div className="contestants__instance">
+                    <img className="contestants__img" src={`${process.env.REACT_APP_SERVER_URL}/${contestant.photo}`} alt="contestant context"/>
+                    <h3 className="contestants__text challenges__name">{contestant.name}</h3>
+                    <h4 className="contestants__text">Season {contestant.season}</h4>
+                    <h4 className="contestants__text">{contestant.career}</h4>
+                </div>
+                </Link>
+                )
+            })
+        }
+        </section>
         </>
     )
 }
