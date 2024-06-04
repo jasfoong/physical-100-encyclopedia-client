11204d8895767f5d6a8ab89b2b589c2dbb6b892f
Jasmine Foong <jasfoong19@gmail.com>
2024-06-04 09:24:15 -0700

    add navbar as slide out burger menu on desktop view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,48 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { slide as Menu } from 'react-burger-menu'
 import homeIcon from "../../assets/logos/home.png"
-import searchIcon from "../../assets/logos/search.png"
+import challengeIcon from "../../assets/logos/challenge.png"
 import './Navbar.scss'
 
 const Navbar = () => {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1280);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth >= 1280);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
+        <>
+        {isDesktop ? (
+            <Menu>
+            <a id="home" className="menu-item" href="/">
+                <img className="navbar__home" src={homeIcon} alt="icon of a house"/>
+                Home</a>
+            <a id="challenges" className="menu-item" href="/about">
+                <img className="navbar__challenge" src={challengeIcon} alt="icon of rocket ship"/>
+                Challenges</a>
+            </Menu>
+        ) : (
         <section className="navbar">
             <Link to="/" className="navbar__home-wrapper">
-                <div >
+                <div>
                     <img className="navbar__home" src={homeIcon} alt="icon of a house"/>
                 </div>
             </Link>
-            <Link to="/search" className="navbar__search-wrapper">
-                <div >
-                    <img className="navbar__search" src={searchIcon} alt="icon of a magnifying glass"/>
+            <Link to="/challenges" className="navbar__challenge-wrapper">
+                <div>
+                    <img className="navbar__challenge" src={challengeIcon} alt="icon of rocket ship"/>
                 </div>
             </Link>
         </section>
+        )}
+        </>
     )
 }
 
