709f1b565c6459a732ca2ed2a9dda6ef5f29db98
Jasmine Foong <jasfoong19@gmail.com>
2024-06-05 15:12:39 -0700

    refactor EditChallengeForm component to remove contestant-related conditionals and props

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -3,90 +3,72 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { axiosInstance } from '../../utils/apiClient'
 import './EditForm.scss'
 
-const EditForm = ({ contestant, challenge, setIsEditing, updateChallenge, updateContestant, editError, setEditError }) => {
-    const navigate = useNavigate();
-    const { id } = useParams();
-    const [inputErrors, setInputErrors] = useState({});
-    const [formData, setFormData] = useState({
-        name: "",
-        description: ""
-    });
+const EditForm = ({ challenge, setIsEditing, updateChallenge, editError, setEditError }) => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [inputErrors, setInputErrors] = useState({});
+  const [formData, setFormData] = useState({
+    name: "",
+    description: ""
+  });
 
-    useEffect(() => {
-        if (contestant) {
-            setFormData({
-                name: contestant.name,
-                description: contestant.description
-            })
-        } else if (challenge) {
-            setFormData({
-                name: challenge.name,
-                description: challenge.description
-            })
-        };
-    }, [contestant, challenge])
-  
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        [name]: value,
-      }));
-    };
-
-    const validateForm = () => {
-        const newErrors = {};
-
-        if (!formData.name) newErrors.name = "Required"
-        if (!formData.description) newErrors.description = "Required"
-
-        setInputErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
+  useEffect(() => {
+    if (challenge) {
+      setFormData({
+          name: challenge.name,
+          description: challenge.description
+      })
     }
+  }, [challenge])
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const validateForm = () => {
+      const newErrors = {};
+
+      if (!formData.name) newErrors.name = "Required"
+      if (!formData.description) newErrors.description = "Required"
+
+      setInputErrors(newErrors);
+      return Object.keys(newErrors).length === 0;
+  }
   
-    const handleFormSubmit = async (e) => {
-      e.preventDefault();
-      
-      if (!validateForm()) {
-        return;
-        }
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    
+    if (!validateForm()) {
+      return;
+      }
 
-      try {
-        if (contestant) {
-            await axiosInstance.put(`/contestants/${id}`, formData);
-            console.log(formData);
-            updateContestant(formData);
-            setIsEditing(false);
-            navigate(`/contestants/${id}`)
-        } else if (challenge) {
-            await axiosInstance.put(`/challenges/${id}`, formData);
-            console.log(formData);
-            updateChallenge(formData);
-            setIsEditing(false);
-            navigate(`/challenges/${id}`)
-        }
-      } catch (error) {
-        console.log(`Could not edit ${contestant ? "contestant" : "challenge"}`, error);
-        setEditError(true);
-      } 
-    };
+    try {
+      await axiosInstance.put(`/challenges/${id}`, formData);
+      console.log(formData);
+      updateChallenge(formData);
+      setIsEditing(false);
+      navigate(`/challenges/${id}`)
+    } catch (error) {
+      console.log(`Could not edit challenge`, error);
+      setEditError(true);
+    } 
+  };
 
-    const handleCancelClick = () => {
-        setIsEditing(false);
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    navigate(`/challenges/${id}`);
+  };
 
-        if (contestant) {
-            navigate(`/contestants/${id}`);
-        } else if (challenge) {
-            navigate(`/challenges/${id}`);
-        }
-    };
-  
-    return (
+  return (
     <>
     <section className="edit-form__section">
-    <div className="edit-form__img-wrapper">
-        <img className="edit-form__img" src={contestant ? `${process.env.REACT_APP_SERVER_URL}/${contestant.photo}` : `${process.env.REACT_APP_SERVER_URL}/${challenge.photo}`} alt={contestant ? "contestant portrait" : "challenge background"}/>
-    </div>
+      <div className="edit-form__img-wrapper">
+          <img className="edit-form__img" src={`${process.env.REACT_APP_SERVER_URL}/${challenge.photo}`} alt={`background of ${challenge.name}`}/>
+      </div>
     <form className="edit-form" onSubmit={handleFormSubmit}>
         <div className="edit-form__name-wrapper">
           <label className="edit-form__label" htmlFor="name">Name</label>
@@ -118,10 +100,10 @@ const EditForm = ({ contestant, challenge, setIsEditing, updateChallenge, update
             <button className="edit-form__btn" type="submit">Publish</button>
             <button className="edit-form__btn" type="button" onClick={handleCancelClick}>Cancel</button>
         </div>
-      </form>
-      </section>
-      </>
-    );
-  };
+    </form>
+    </section>
+    </>
+  );
+};
   
-  export default EditForm;
+export default EditForm;
