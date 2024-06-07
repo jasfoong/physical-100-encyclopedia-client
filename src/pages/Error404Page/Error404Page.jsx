0b36163c3035b49b7e3b9b4b9546a546fc73305a
Jasmine Foong <jasfoong19@gmail.com>
2024-06-07 07:46:56 -0700

    add burger menu navbar to error 404 page

--- src/pages/Error404Page/Error404Page.jsx
+++ src/pages/Error404Page/Error404Page.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar"
 import "./Error404Page.scss"
 
 const Error404Page = () => {
     return (
         <main className="error404">
+            <Navbar />
             <h1 className="error404__heading">ERROR 404</h1>
             <h2 className="error404__text">Welcome to the Physical 100 training grounds.</h2>
             <h2 className="error404__text">Stretch those navigation muscles on your way back to the main show.</h2>
